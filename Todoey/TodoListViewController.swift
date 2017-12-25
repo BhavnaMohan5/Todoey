93698de9ee6928728fcbfb1b62c6c772c1666373
Bhavna Mohan <bhavnamohan5@gmail.com>
2017-12-25 13:39:09 +0530

    Persistence using user defaults done

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -11,9 +11,15 @@ import UIKit
 class TodoListViewController: UITableViewController {
 
     var itemArray = ["Find Mike","Get Eggs","Destroy Demogorgon"]
-    
+    let defaults = UserDefaults.standard
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let items = defaults.array(forKey : "ToDoListArray") as? [String]
+        {
+            itemArray = items
+        }
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -54,6 +60,7 @@ class TodoListViewController: UITableViewController {
         let action = UIAlertAction(title : "Add Item", style : .default) { (action) in
            // print(textFiled.text)
             self.itemArray.append(textFiled.text!)
+            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
             self.tableView.reloadData()
         }
         alert.addTextField { (alertTextField) in
